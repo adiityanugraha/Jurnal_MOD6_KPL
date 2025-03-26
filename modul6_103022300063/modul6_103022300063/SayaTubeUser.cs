8e5f784fb61c2aca6f9e002ac09768630ff213fb
adiityanugraha <150656990+adiityanugraha@users.noreply.github.com>
2025-03-26 07:16:59 +0700

    Add files via upload
    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_103022300063/modul6_103022300063/SayaTubeUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_103022300063
+{
+    class SayaTubeUser
+    {
+        private int id;
+        private string username;
+        private List<SayaTubeVideo> uploadedVideos;
+
+        public SayaTubeUser(String username)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(1000, 9999);
+            this.username = username;
+            this.uploadedVideos = new List<SayaTubeVideo>();
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            uploadedVideos.Add(video);
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int total = 0;
+            foreach (var video in uploadedVideos)
+            {
+                total += video.playCount;
+            }
+            return total;
+        }
+
+        public void PrinAllVideoPlaycount()
+        {
+            Console.WriteLine($"User : {username}");
+            int count = 1;
+            foreach (var video in uploadedVideos)
+            {
+                Console.WriteLine($"Video {count} : {video.title}");
+                count++;
+            }
+        }
+    }
+}
--- modul6_103022300063/modul6_103022300063/obj/Debug/net8.0/modul6_103022300063.AssemblyInfo.cs
+++ modul6_103022300063/modul6_103022300063/obj/Debug/net8.0/modul6_103022300063.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul6_103022300063")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73849ee47b7ce7f21e48e759fab4d208294ea88")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul6_103022300063")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul6_103022300063")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
